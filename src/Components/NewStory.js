4cbe48ffae1d8c094382a8dadef33e16c912d7d4
maihoangminhbk <61836935+maihoangminhbk@users.noreply.github.com>
2022-06-10 08:53:03 +0700

    Change post new story

--- src/Components/NewStory.js
+++ src/Components/NewStory.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import uuidv1 from 'uuid/v1';
-import {Editor, EditorState, RichUtils, getDefaultKeyBinding} from 'draft-js';
+import {Editor, EditorState} from 'draft-js';
 import {stateToHTML} from 'draft-js-export-html';
 
 import BlockStyleControls from './RichText/BlockStyleControls';
@@ -20,10 +20,6 @@ export default class NewStory extends Component {
 
       this.focus = () => this.refs.editor.focus();
       this.onChange = this.onChange.bind(this);
-      this.handleKeyCommand = this._handleKeyCommand.bind(this);
-      this.mapKeyToEditorCommand = this._mapKeyToEditorCommand.bind(this);
-      this.toggleBlockType = this._toggleBlockType.bind(this);
-      this.toggleInlineStyle = this._toggleInlineStyle.bind(this);
       this.handleSubmit = this.handleSubmit.bind(this);
     }
 
@@ -31,51 +27,10 @@ export default class NewStory extends Component {
       this.setState({editorState});
       const contentState = this.state.editorState.getCurrentContent();
       const html = stateToHTML(contentState);
-      this.state.title && this.state.body ? this.setState({done: 'done'}) : console.log('Made by Zonayed Ahmed, My portfolio: https://www.zonayed.me');
+      this.state.title && this.state.body ? this.setState({done: 'done'}) : console.log('Made by Group 2');
       this.setState({body: html});
     }
 
-    _handleKeyCommand(command, editorState) {
-      const newState = RichUtils.handleKeyCommand(editorState, command);
-      if (newState) {
-        this.onChange(newState);
-        return true;
-      }
-      return false;
-    }
-
-    _mapKeyToEditorCommand(e) {
-      if (e.keyCode === 9 /* TAB */) {
-        const newEditorState = RichUtils.onTab(
-          e,
-          this.state.editorState,
-          4, /* maxDepth */
-        );
-        if (newEditorState !== this.state.editorState) {
-          this.onChange(newEditorState);
-        }
-        return;
-      }
-      return getDefaultKeyBinding(e);
-    }
-
-    _toggleBlockType(blockType) {
-      this.onChange(
-        RichUtils.toggleBlockType(
-          this.state.editorState,
-          blockType
-        )
-      );
-    }
-
-    _toggleInlineStyle(inlineStyle) {
-      this.onChange(
-        RichUtils.toggleInlineStyle(
-          this.state.editorState,
-          inlineStyle
-        )
-      );
-    }
 
     handleSubmit() {
       const { title, body } = this.state;
@@ -101,20 +56,9 @@ export default class NewStory extends Component {
       return (
         <div className="richtext-editor">
           <input type="text" className="input-title" value={title} onChange={(e) => this.setState({title: e.target.value})} placeholder="タイトル" />
-          <BlockStyleControls
-            editorState={editorState}
-            onToggle={this.toggleBlockType}
-          />
-          <InlineStyleControls
-            editorState={editorState}
-            onToggle={this.toggleInlineStyle}
-          />
           <div className={className} onClick={this.focus}>
             <Editor
-              blockStyleFn={getBlockStyle}
               editorState={editorState}
-              handleKeyCommand={this.handleKeyCommand}
-              keyBindingFn={this.mapKeyToEditorCommand}
               onChange={this.onChange}
               placeholder="ストーリーを話してください。。。"
               ref="editor"
@@ -125,11 +69,5 @@ export default class NewStory extends Component {
         </div>
       );
 
-      function getBlockStyle(block) {
-        switch (block.getType()) {
-          case 'blockquote': return 'RichEditor-blockquote';
-          default: return null;
-        }
-      }
     }
 }
